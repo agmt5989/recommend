d9ec6ec02911e0c0c617f883f0807b51cc2b0cb8
Mike Ajala <agmt5989@gmail.com>
2019-03-02 10:42:56 +0100

    setting the views directory

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.engine('handlebars', hbs({
 }));
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
 //	res.json({message: 'Well, we seem to have made it!'});
