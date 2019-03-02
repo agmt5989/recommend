fd08fed7c1ed8d04c05548a81de9ee79c58990dd
Mike Ajala <agmt5989@gmail.com>
2019-03-02 11:31:55 +0100

    the /end endpoint

--- app.js
+++ app.js
@@ -44,6 +44,23 @@ app.get('/start', (req, res) => {
 	});
 });
 
+app.get('/end', (req, res) => {
+	exec('python app.py ' + req.query.id, (err, stdout, stderr) => {
+		if (err) {
+			console.log("Couldn't execute");
+			res.json({errpr: true, message: 'Failed to execute command.'});
+		} else if (stdout) {
+			// the *entire* stdout and stderr (buffered)
+			console.log(`stdout: ${stdout}`);
+			res.json({response: stdout});
+		} else {
+			console.log(`stderr: ${stderr}`);
+			res.json({response: stderr});
+		}
+
+	});
+});
+
 app.listen(3000, () => {
 	console.log('We have arrived');
 });
