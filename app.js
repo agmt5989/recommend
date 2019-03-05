e7e397b9e601a20243d76923354c781149f362f0
Mike Ajala <agmt5989@gmail.com>
2019-03-05 07:09:33 +0100

    everything again, with new insight

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/end', (req, res) => {
 			let resArray = stdout.split('\n');
 			let result = resArray[2];
 			let bulk = result.replace(/[()]/g,'').split("]', '[");
-			result = "{real: " + bulk[0] + "]', recommend: '[" + bulk[1] + "}";
+			result = "{'real': " + bulk[0] + "]', 'recommend': '[" + bulk[1] + "}";
 			result = JSON.parse(result);
 			console.log(result);
 //			console.log(`stdout: ${resArray}`);
