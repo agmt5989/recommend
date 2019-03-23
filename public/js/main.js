301d47a8234d4edbe0bdc7bca3ba5372f6c974ef
Mike Ajala <agmt5989@gmail.com>
2019-03-23 12:34:05 +0100

    debug returned payload

--- public/js/main.js
+++ public/js/main.js
@@ -21,8 +21,8 @@
 	$('#finder').submit(function (e) {
 		$.post('/end', {id: $('#findid').val(), no: $('#findno').val()}, function (result) {
 			console.log('Submitted!');
-				if (result.success && result.session) {
-					window.location.replace('/go');
+				if (result.success) {
+					console.log(result.message);
 				}
 			}
 		);
